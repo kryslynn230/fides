4ba0d9ec71ff44db5fc9bd9d221b8bc161fff349
Nicolas Fry <nicolas@ethyca.com>
2023-03-29 17:41:08 -0400

    feat: added GTM Tag manager checks using env vars

--- src/fides/data/sample_project/cookie_house/src/pages/index.tsx
+++ src/fides/data/sample_project/cookie_house/src/pages/index.tsx
@@ -14,6 +14,8 @@ export const getServerSideProps: GetServerSideProps<Props> = async () => {
   return { props: { products: results.rows } };
 };
 
+const GTMContainerID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_CONTAINER
+
 const IndexPage = ({
   products,
 }: Props) => {
@@ -24,7 +26,24 @@ const IndexPage = ({
         <meta name="description" content="Sample Project used within Fides (github.com/ethyca/fides)" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
+        {/* Google Tag Manager */}
+        {GTMContainerID ? <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTMContainerID}');`,
+          }}
+        /> : ""}
       </Head>
+
+      {GTMContainerID ? <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTMContainerID}"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+        }}
+      /> : ""}
 
       <Home products={products} />
     </>
