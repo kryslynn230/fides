b07f9566e4e0d6a206b23a605394ca84b8da831b
Nicolas Fry <nicolas@ethyca.com>
2023-03-30 14:45:47 -0400

    fix: GTM loading

--- src/fides/data/sample_project/cookie_house/src/pages/index.tsx
+++ src/fides/data/sample_project/cookie_house/src/pages/index.tsx
@@ -1,3 +1,4 @@
+// import TagManager from 'react-gtm-module';
 import { Product } from '../types';
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
@@ -12,8 +13,6 @@ interface Props {
 }
 
 
-console.log(process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_CONTAINER);
-
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
   const results = await pool.query<Product>('SELECT * FROM public.product;');
   return { props: { products: results.rows, gtm_key: process.env.GOOGLE_TAG_MANAGER_CONTAINER } };
@@ -25,11 +24,10 @@ const IndexPage = ({
   gtm_key,
 }: Props) => {
 
-  if (gtm_key) {
-    console.log('Google Tag Manager Key', gtm_key);
-  } else {
-    console.log('Google Tag Manager Key not found');
-  }
+  // if (gtm_key) {
+  //   console.log('GTM Key found: ', gtm_key);
+  //   TagManager.initialize({ gtmId: gtm_key });
+  // }
 
   return (
     <>
@@ -38,28 +36,20 @@ const IndexPage = ({
         <meta name="description" content="Sample Project used within Fides (github.com/ethyca/fides)" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
-        {/* Google Tag Manager */}
-        {gtm_key && <Script id="google-tag-manager" strategy="afterInteractive">
-          {
-            `
-            (function (w, d, s, l, i) {
-            w[l] = w[l] || []; w[l].push({
-              'gtm.start':
-                new Date().getTime(), event: 'gtm.js'
-            }); var f = d.getElementsByTagName(s)[0],
-              j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-          })(window, document, 'script', 'dataLayer', '${gtm_key}');`}
-        </Script> }
       </Head>
-
-      {gtm_key && <noscript
-        dangerouslySetInnerHTML={{
-          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${gtm_key}"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-        }}
-      /> }
-
+      {gtm_key && (
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function (w, d, s, l, i) {
+              w[l] = w[l] || []; w[l].push({
+                'gtm.start':
+                  new Date().getTime(), event: 'gtm.js'
+              }); var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+                  'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+            })(window, document, 'script', 'dataLayer', '${gtm_key}');
+          `}
+        </Script>)}
       <Home products={products} />
     </>
   );
